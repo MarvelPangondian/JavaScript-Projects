478a02d6e9c0fa39eabec86b0afda9b72c2011ff
MarvelPangondian <marvel.pangondian01@gmail.com>
2024-08-21 16:38:04 +0700

    feat: done with todo project

--- todo-project/script.js
+++ todo-project/script.js
@@ -1,25 +1,46 @@
-// let todoList = [];
-// const todoNameInput = document.querySelector('.todo-name-input');
-// const addTodoElm = document.querySelector('.js-add-todo');
-// const todoListContainerElm = document.querySelector('.js-todo-list-container');
-// const 
-// addTodoElm.addEventListener('click', () => {
-//     if (todoNameInput.value){
-//         todoList.push(todoNameInput.value);
-//      } else {
-//         return;
-//      }
-//     let output = '';
-//     todoList.forEach((element,index) => {
-//         output += `<p class='todo-list-name'>${element}</p> <p class='date'>${} </p> <button>Delete</button>`
-//     });
-//     todoListContainerElm.innerHTML = output;
-// });
+let allTodo = JSON.parse(localStorage.getItem('todo')) ||  [{
+}];
 
-
-
-let allTodo = []
 
 const todoNameInput = document.querySelector('.js-todo-name-input');
 const dateInput = document.querySelector('.js-date-input');
 const addTodoButton = document.querySelector('.js-add-todo-button');
+const todoGridElement = document.querySelector('.js-todo-list');
+updateTodoGrid();
+
+const deleteTodoButton = document.querySelectorAll('.js-delete-todo-button');
+
+
+addTodoButton.addEventListener('click', () => {
+    if (!todoNameInput.value) {
+        return;
+    }
+
+    allTodo.push( {
+        todoName: todoNameInput.value,
+        date : dateInput.value
+    });
+    console.log(allTodo);
+    updateTodoGrid();
+});
+
+todoGridElement.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('delete-todo-button') ){
+        return;
+    }
+    allTodo.splice(event.target.dataset.index, 1);
+    updateTodoGrid();
+})
+
+function updateTodoGrid(){
+    let output = '';
+    allTodo.forEach((element,index) => {
+        output += `<p class="todo-name">${element.todoName}</p> <p class="todo-date">${element.date}</p> <button class="delete-todo-button js-delete-todo-button" data-index='${index}'>Delete</button>`;
+    });
+    todoGridElement.innerHTML = output;
+    todoNameInput.value = '';
+    dateInput.value = '';
+
+    localStorage.setItem('todo',JSON.stringify(allTodo));
+}   
+
