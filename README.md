fb71f7422c6a2366bc5d69a6793f1c4e079d6e2b
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-08-28 22:21:08 +0700

    feat: successfully call api, update readme

--- README.md
+++ README.md
@@ -13,14 +13,27 @@
 - CSS
 
 ## Screenshots
+
+### Most recent: Weather App
+- A responsive web that uses [Weather](https://openweathermap.org/current) API. This web will change appearance based on current time by using [dayjs](https://www.npmjs.com/package/dayjs)
+
+![night time](./screenshots/wa1.png)</br></br>
+![noon](./screenshots/wa2.png)
+
+### Todo project
+- A simple Todo project that uses javascript list and localStorage to save and display data based on user input 
+
+![initial state](./screenshots/todo1.png)</br></br>
+![example](./screenshots/todo2.png)
+
 ### Rock Paper Scissors game
+- A simple rock paper scissors game that utilises javascript listeners and localStorage to keep score
+
 ![initial state](./screenshots/rps1.png)</br></br>
 ![example](./screenshots/rps2.png)
 
 ### Calculator project
+- A simple calculator project that displays the numbers dynamically as the user types it out 
+
 ![initial state](./screenshots/calc1.png)</br></br>
 ![example](./screenshots/calc2.png)
-
-### Todo project
-![initial state](./screenshots/todo1.png)</br></br>
-![example](./screenshots/todo2.png)
---
+++ weather-app/script.js
@@ -0,0 +1,63 @@
+// fetch(
+//   `https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=42c63b6dd6ece1f3ccd8573d7cf6b101`
+// )
+//   .then((response) => response.json())
+//   .then((response) => {
+//     console.log(response);
+
+//   });
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+
+
+let currentCity = 'Jakarta';
+generateWeather(currentCity);
+
+async function generateWeather(city){
+  try {
+    let today;
+    let hour;
+    let night = false;
+    const card =  document.querySelector('.js-card');
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=42c63b6dd6ece1f3ccd8573d7cf6b101`);
+    const data = await response.json();
+
+    today = dayjs();
+    hour = today.hour();
+    console.log(hour);
+    if (hour > 18 || hour < 6){
+      card.classList.remove('card-night');
+      card.classList.add('card-night');
+      night = true;
+    } else {
+      card.classList.remove('card-night');
+    }
+
+    document.querySelector('.js-temperature').innerText = kelvinToCelcius(data.main.temp);
+    document.querySelector('.js-weather').innerText = data.weather[0].main;
+    let extraDetail = night ? '-night' : '';
+    document.querySelector('.js-weather-icon').src = `./assets/${data.weather[0].main.toLowerCase() + extraDetail}.png`;
+    document.querySelector('.js-city-name').innerText = data.name;
+    document.querySelector('.js-humidity-quantity').innerText = data.main.humidity + "%";
+    document.querySelector('.js-wind-quantity').innerText = data.wind.speed + "m/s";
+    
+  
+  } catch(error) {
+    alert("Something went wrong...");
+  }
+  
+
+}
+
+function kelvinToCelcius(temp){
+  return (Math.round(temp*100 - 27315)/100).toFixed(1);
+}
+
+document.querySelector('.js-input-search').addEventListener('keydown', function(event){
+  if (event.key === 'Enter'){
+    currentCity = this.value;
+    console.log(currentCity);
+    this.value = '';
+    this.blur();
+    generateWeather(currentCity);
+  }
+});
